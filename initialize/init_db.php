83e32ab18788e00aecd1c1c031ac1322ba3cde1a
Sphenox <pfister.tim@hotmail.ch>
2016-12-13 10:35:02 +0100

    Init

--- initialize/init_db.php
+++ initialize/init_db.php
@@ -11,6 +11,8 @@ if ($db->connect_error) {
     die("Connection failed: " . $db->connect_error);
 }
 
+$db->query("DROP DATABASE journey");
+
 $createDB = "CREATE DATABASE IF NOT EXISTS journey";
 
 if ($db->query($createDB) === TRUE) {
