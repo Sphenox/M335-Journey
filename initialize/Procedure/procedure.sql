a0fef1fe3344827d3b3ec627f0807bf297db1582
Utschu <arjuna1997@windowslive.com>
2016-12-16 13:10:25 +0100

    Procedure

--- initialize/Procedure/procedure.sql
+++ initialize/Procedure/procedure.sql
@@ -20,27 +20,35 @@ BEGIN
 	WHERE loc.FKuser = u.id
 	AND u.id = inId;
 END //
-#no nid fertig
 CREATE PROCEDURE getAllVisibleLocation
-(IN inId INT(11), IN visibility VARCHAR(10))
+(IN inId INT(11))
 BEGIN
 	SELECT loc.* FROM journey.users as u , journey.locations as loc
-	WHERE loc.FKuser = u.id
-	AND u.id = inId;
+	WHERE u.id = inId
+	AND ((loc.FKuser = u.id)
+	OR (loc.visible = "all")
+	OR (loc.visible = "friends" AND loc.FKuser IN (SELECT u.id FROM journey.users as u RIGHT JOIN journey.friends as f ON ((inId = f.FKuser1 AND f.FKuser2 = u.id ) OR ( inId = f.Fkuser2 AND f.FKuser1 = u.id))WHERE u.id != inId)));
 END //
-#no nid fertig
 CREATE PROCEDURE getFriendsVisibleLocation
-(IN inId INT(11), IN visibility VARCHAR(10))
+(IN inId INT(11))
 BEGIN
-	SELECT loc.* FROM journey.users as u , journey.locations as loc
-	WHERE loc.FKuser = u.id
-	AND u.id = inId;
+SELECT loc.* FROM journey.users as u , journey.locations as loc
+	WHERE u.id = inId
+	AND ((loc.FKuser = u.id)
+	OR (loc.visible = "friends" AND loc.FKuser IN (SELECT u.id FROM journey.users as u RIGHT JOIN journey.friends as f ON ((inId = f.FKuser1 AND f.FKuser2 = u.id ) OR ( inId = f.Fkuser2 AND f.FKuser1 = u.id));
 END //
-CREATE PROCEDURE getUser
-(IN inEmail VARCHAR(255), IN inPassword VARCHAR(255))
+#Goht no nid
+CREATE PROCEDURE checkUserLogin
+(IN inEmail VARCHAR(255), IN inPassword VARCHAR(255) , OUT outID INT(11))
 BEGIN
-	SELECT u.* FROM journey.users as u
+	SELECT IF(COUNT(u.*) > 0 , 'true' , 'false') , u.id INTO outID FROM journey.users as u
 	WHERE u.email = inEmail
-	AND u.password = inPassword
+	AND u.password = inPassword;
+END //
+CREATE PROCEDURE checkIfEmailExist
+(IN inEmail VARCHAR(255))
+BEGIN
+	SELECT IF(COUNT(u.*) > 0 , 'true' , 'false') FROM journey.users as u
+	WHERE u.email = inEmail;
 END //
 DELIMITER ;
